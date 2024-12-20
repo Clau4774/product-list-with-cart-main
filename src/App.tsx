ff1d93b53fbdbcf61d6bf84a3b0de523de32b348
claudioDang47 <claudiodangiolo@gmail.com>
2024-12-20 17:00:54 -0300

    Remove Card component and add Cart Component for functionality test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import React from "react";
 import "./App.css";
-import Card from "./components/Card";
 import data from "./data/data.json";
+import Cart from "./components/cart/Cart";
+import { CartItemType } from "./interfaces";
 
 function App() {
+  const item = {
+    name: data[0].name,
+    price: data[0].price,
+    quantity: 1,
+  };
+
+  const [cart, setCart] = React.useState([]);
+
+  setCart([...cart, item]);
   return (
     <div>
-      {data.map((item, index) => (
-        <Card key={index} {...item} />
-      ))}
+      <Cart cart={cart} setCart={setCart} />
     </div>
   );
 }
