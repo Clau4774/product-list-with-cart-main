5b49ee4f2f9573346803c74fe03cc9bdafc0f0c7
claudioDang47 <claudiodangiolo@gmail.com>
2024-12-20 16:59:30 -0300

    Creat CardItem Component

---
+++ src/components/cart/CartItem.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { CartItemType } from "../../interfaces";
+import closeButton from "../../assets/images/icon-remove-item.svg";
+
+function CartItem({ itemName, itemPrice, quantity, setCart }: CartItemType) {
+  const totalPrice = itemPrice * quantity;
+
+  const RemoveItem = () => {
+    setCart((prev: any) => {
+      return prev.filter((item: any) => item.itemName !== itemName);
+    });
+  };
+
+  return (
+    <div>
+      <div>
+        <h3>{itemName}</h3>
+        <div>
+          <p>
+            {quantity}x <span>{itemPrice}</span> <span>{totalPrice}</span>
+          </p>
+        </div>
+      </div>
+      <div>
+        <button>
+          <img src={closeButton} alt="CloseButton" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CartItem;
