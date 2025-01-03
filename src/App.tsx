3a429d082f212294cdb07a8c58c1905c29653339
claudioDang47 <claudiodangiolo@gmail.com>
2025-01-03 13:54:20 -0300

    Comment import React, and add background to div containing Cart Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.css";
 import Cart from "./components/cart/Cart";
-import { CartItemType } from "./interfaces";
 
 function App() {
   return (
-    <div>
-      <Cart cart={cart} setCart={setCart} />
+    <div className="bg-rose-50">
+      <Cart />
     </div>
   );
 }
