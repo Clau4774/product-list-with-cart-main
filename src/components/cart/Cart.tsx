841f0d9a954b7e46f3f4105690e95bf8f3d34310
claudioDang47 <claudiodangiolo@gmail.com>
2025-01-03 14:04:09 -0300

    Use context and useCart to manage the Cart functionality. Create cartItems for test, create index state for add products for testing

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { CartItemType } from "../../interfaces";
 import CartItem from "./CartItem";
+import { AppContext } from "../../context/AppContext";
 
-function Cart({ cart, setCart }: { cart: any; setCart: any }) {
+function Cart() {
+  const { data, addElement, cart } = React.useContext(AppContext);
+
+  const [index, setIndex] = React.useState(0);
+
+  const item = {
+    id: index,
+    name: data[index].name,
+    price: data[index].price,
+    quantity: 1,
+  };
+
   return (
     <div>
       {cart.map((item: CartItemType, index: number) => (
