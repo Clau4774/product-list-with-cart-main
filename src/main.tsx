62c4dc8a7a866e360e00d9af471f666bf68647f6
claudioDang47 <claudiodangiolo@gmail.com>
2025-01-03 13:54:56 -0300

    Change provider name

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import Provider from "./contexts And Provider/Provider.tsx";
+import ContextProvider from "./context/ContextProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
+    <ContextProvider>
       <App />
-    </Provider>
+    </ContextProvider>
   </StrictMode>
 );
